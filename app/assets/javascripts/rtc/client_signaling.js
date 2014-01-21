b7f87a952df33a52d992eb7307c59683162a1f77
xescuGC <xescugil@gmail.com>
2014-01-21 23:49:19 +0100

    First funcional implementation

--- app/assets/javascripts/rtc/client_signaling.js
+++ app/assets/javascripts/rtc/client_signaling.js
@@ -2,18 +2,37 @@ var socket = io.connect('127.0.0.1:2013');
 
 var room = "room-name" //prompt("Enter room name:");
 
-var isInitializer
-
-socket.on("created", function (){
-  console.log("On Created");
-  isInitializer = true;
-})
-
-socket.on("joinded", function(){
-  console.log("On Joinded");
-  isInitializer = false;
-})
-
-socket.on("full", function(){
-  console.alert("Room Full, max number of 2");
-})
+  socket.on("created", function (){
+    console.log("On Created");
+    isInitiator = true;
+    console.log('isInitiator', isInitiator);
+  })
+  
+  socket.on("joined", function(){
+    console.log("Some one Joinded");
+    isChannelReady = true;
+  })
+  
+  socket.on("full", function(){
+    console.alert("Room Full, max number of 2");
+  })
+  
+  socket.on("message", function(data) {
+    if (data.type === 'gotStream') {
+      console.log(data.message);
+      maybeStart();
+    }else if (data.type === 'candidate' && isStarted) {
+      var candidate = new RTCIceCandidate({
+        candidate: data.message.candidate
+      });
+      pc.addIceCandidate(candidate);  
+    }else if (data.type === 'answer' && isStarted) {
+      pc.setRemoteDescription(new RTCSessionDescription(data.message));
+    }else if (data.type === 'offer') {
+      if (!isInitiator && !isStarted) {
+        maybeStart();
+      }   
+      pc.setRemoteDescription(new RTCSessionDescription(data.message));
+      startAnswer();
+    }
+  })
--- app/rtc/server.js
+++ app/rtc/server.js
@@ -6,29 +6,34 @@ var io = require('socket.io').listen(app);
 
 io.sockets.on('connection', function (socket){
 
-  function log(){
-    var array = [">>> Message from server: "];
-    for (var i = 0; i < arguments.length; i++) {
-      array.push(arguments[i]);
-    }
-    socket.emit('log', array);
-  }
+  // function log(){
+  //   var array = [">>> Message from server: "];
+  //   for (var i = 0; i < arguments.length; i++) {
+  //     array.push(arguments[i]);
+  //   }
+  //   socket.emit('log', array);
+  // }
 
-  socket.on('message', function (message) {
-    log('Got message:', message);
+  socket.on('message', function (data) {
+    // log('Got message:', data.message);
     // for a real app, would be room only (not broadcast)
-    socket.broadcast.emit('message', message);
+    io.sockets.in(data.room).emit('message', data);
+    // socket.broadcast.emit('message', message);
   });
 
   socket.on('create or join', function (room) {
     var numClients = io.sockets.clients(room).length;
+
+    console.log('clients', numClients);
 
     if(numClients == 0){
       socket.join(room);
       socket.emit('created');
     } else if (numClients == 1){
       socket.join(room);
-      socket.emit('joined');
+      io.sockets.in(room).emit('joined');
+      // socket.broadcast.to(room).emit('join');
+      // socket.emit('joined');
     } else {
       socket.emit('full');
     }
