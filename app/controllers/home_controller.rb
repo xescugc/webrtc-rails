99f08dcd408abab8e21e251c8ec8a90f0ad15195
xescuGC <xescugil@gmail.com>
2014-01-22 22:57:06 +0100

    Refractored the Interface

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,6 @@ class HomeController < ApplicationController
   def index
   end
 
-  def webcam_audio_video
-  end
-
-  def webrtc_same_page
-  end
-
-  def stream_arbitrary_data
-  end
-
   def client_to_client
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 WebrtcRails::Application.routes.draw do
   root to: 'home#index'
 
-  get '/webcam_audio_video'     =>  'home#webcam_audio_video'  
-  get '/webrtc_same_page'       =>  'home#webrtc_same_page' 
-  get '/stream_arbitrary_data'  =>  'home#stream_arbitrary_data' 
   get '/client_to_client'       =>  'home#client_to_client' 
 
 end
