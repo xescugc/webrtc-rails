af62971f36058393ba2949eb790bebc41a07ecbf
xescuGC <xescugil@gmail.com>
2014-01-22 21:56:24 +0100

    Moved the socket events to diferent file, client_signaling.js

--- app/assets/javascripts/rtc/client_signaling.js
+++ app/assets/javascripts/rtc/client_signaling.js
@@ -1,38 +1,38 @@
-var socket = io.connect('127.0.0.1:2013');
+var socket = io.connect('192.168.1.25:2013');
 
 var room = "room-name" //prompt("Enter room name:");
 
-  socket.on("created", function (){
-    console.log("On Created");
-    isInitiator = true;
-    console.log('isInitiator', isInitiator);
-  })
-  
-  socket.on("joined", function(){
-    console.log("Some one Joinded");
-    isChannelReady = true;
-  })
-  
-  socket.on("full", function(){
-    console.alert("Room Full, max number of 2");
-  })
-  
-  socket.on("message", function(data) {
-    if (data.type === 'gotStream') {
-      console.log(data.message);
+socket.on("created", function (){
+  console.log("On Created");
+  isInitiator = true;
+  console.log('isInitiator', isInitiator);
+})
+
+socket.on("joined", function(){
+  console.log("Some one Joinded");
+  isChannelReady = true;
+})
+
+socket.on("full", function(){
+  console.alert("Room Full, max number of 2");
+})
+
+socket.on("message", function(data) {
+  if (data.type === 'gotStream') {
+    console.log(data.message);
+    maybeStart();
+  }else if (data.type === 'candidate' && isStarted) {
+    var candidate = new RTCIceCandidate({
+      candidate: message.message
+    });
+    pc.addIceCandidate(candidate);  
+  }else if (data.type === 'answer' && isStarted) {
+    pc.setRemoteDescription(new RTCSessionDescription(data.message));
+  }else if (data.type === 'offer') {
+    if (!isInitiator && !isStarted) {
       maybeStart();
-    }else if (data.type === 'candidate' && isStarted) {
-      var candidate = new RTCIceCandidate({
-        candidate: data.message.candidate
-      });
-      pc.addIceCandidate(candidate);  
-    }else if (data.type === 'answer' && isStarted) {
-      pc.setRemoteDescription(new RTCSessionDescription(data.message));
-    }else if (data.type === 'offer') {
-      if (!isInitiator && !isStarted) {
-        maybeStart();
-      }   
-      pc.setRemoteDescription(new RTCSessionDescription(data.message));
-      startAnswer();
-    }
-  })
+    }   
+    pc.setRemoteDescription(new RTCSessionDescription(data.message));
+    startAnswer();
+  }
+})
